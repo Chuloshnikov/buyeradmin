df52c45210cf4e4e7e20209ee9be4f3a65c4901a
MaxChuloshnikov <Maks447@ukr.net>
2023-04-29 17:05:40 +0300

    Autentification logic done

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from "axios";
 
 const Layout = ({ children }) => {
+    const [login, setLogin] = useState(false);
+    const [username, setUsername] = useState(null);
+    const [password, setPassword] = useState(null);
+    const [error, setError] = useState(false);
+
+
+    const handleClick = async () => {
+        try {
+            await axios.post("http://localhost:3000/api/login", {username, password});
+            setLogin(true);
+        } catch(err) {
+            console.log(error);
+        }
+    }
+
   return (
     <>
     <div className='bg-white w-screen h-screen flex items-center'>
         <div className='flex flex-col items-center text-center w-full'>
-            <div className='border p-5'>
+            <div className='border p-5 shadow-containerShadow'>
                 <h2 className='text-orange-400 text-bold text-2xl mb-2'>Welcome!</h2>
                 <form className='flex flex-col gap-2'>  
-                <input type="text" placeholder='Enter your username...'></input>
-                <input type="password" placeholder='Enter your password...'></input>
+                <input 
+                onChange={(e) => setUsername(e.target.value)}
+                type="text" 
+                placeholder='Enter your username...'
+                />
+                <input 
+                onChange={(e) => setPassword(e.target.value)}
+                type="password" 
+                placeholder='Enter your password...'
+                />
                 <button 
+                    onClick={handleClick}
                     className='bg-orange-400 text-white p-1 px-2 text-base font-semibold
                         hover:scale-105 duration-300
                         '>
---
+++ pages/api/login.js
@@ -0,0 +1,21 @@
+import cookie from "cookie";
+
+export default function handler (req, res) {
+    if(req.method === "POST") {
+        const { username, password } = req.body;
+        if (username === process.env.USERNAME && password === process.env.PASSWORD) {
+            res.setHeaders(
+                "Set-Cookie", 
+                cookie.serialize("token", process.env.TOKEN, {
+                    maxAge: 60 * 60,
+                    sameSite: "strict",
+                    path: "/",
+                })
+            );
+            res.status(200).json("Succesfull");
+        } else {
+            res.status(400).json("Wrong Credentials!");
+        }
+    }
+};
+

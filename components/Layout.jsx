44d7473b6f98dfb1a6f711870092e2d6b926b998
MaxChuloshnikov <Maks447@ukr.net>
2023-07-25 12:43:39 +0300

    done

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,73 +2,70 @@ import Navbar from '@/components/Navbar';
 import React, { useState } from 'react';
 import { useSession, signIn, signOut } from "next-auth/react";
 
-
-
-
 const Layout = ({ children }) => {
-    const [showNav, setShowNav] = useState(false);
-    const [username, setUserName] = useState();
-    const [password, setPassword] = useState();
-    const {data: session } = useSession();
-    
+  const [showNav, setShowNav] = useState(false);
+  const [username, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+  const { data: session } = useSession();
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        signIn('credentials', { username, password});
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await signIn('credentials', { username, password });
+    } catch (error) {
+        console.error('Помилка авторизації:', error.message);
+        return error.message;
     }
+  };
 
-
-    if (!session) {
-                return (
-                    <>
-                        <div className='bg-white w-screen h-screen flex items-center'>
-                            <div className='flex flex-col items-center text-center w-full'>
-                                <div className='border p-5 shadow-containerShadow'>
-                                    <h2 className='text-orange-400 text-bold text-2xl mb-2'>Welcome!</h2>
-                                    <form onSubmit={handleSubmit} className='flex flex-col gap-2'>  
-                                    <input 
-                                    onChange={e => setUserName(e.target.value)}
-                                    value={username}
-                                    type="text"
-                                    name="username" 
-                                    placeholder='Enter your username...'
-                                    required
-                                    />
-                                    <input
-                                    onChange={e => setPassword(e.target.value)}
-                                    value={password}
-                                    type="password" 
-                                    name="password"
-                                    placeholder='Enter your password...'
-                                    required
-                                    />
-                                    <button 
-                                        type="submit"
-                                        className='bg-orange-400 text-white p-1 px-2 text-base font-semibold
-                                            hover:scale-105 duration-300
-                                            '>
-                                            Login
-                                    </button>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
-                    </>
-                )
-    }
-
-        return (
-            <div className='bg-orange-400 min-h-screen flex'>
-                
-                <Navbar showNav={showNav} setShowNav={setShowNav}/>
-                <div className='bg-white flex-grow p-4'>
-                {children}
-                </div>
+  if (!session) {
+    return (
+      <>
+        <div className='bg-white w-screen h-screen flex items-center'>
+          <div className='flex flex-col items-center text-center w-full'>
+            <div className='border p-5 shadow-containerShadow'>
+              <h2 className='text-orange-400 text-bold text-2xl mb-2'>Welcome!</h2>
+              <form onSubmit={handleSubmit} className='flex flex-col gap-2'>
+                <input
+                  onChange={e => setUserName(e.target.value)}
+                  value={username}
+                  type="text"
+                  name="username"
+                  placeholder='Enter your username...'
+                  required
+                />
+                <input
+                  onChange={e => setPassword(e.target.value)}
+                  value={password}
+                  type="password"
+                  name="password"
+                  placeholder='Enter your password...'
+                  required
+                />
+                <button
+                  type="submit"
+                  className='bg-orange-400 text-white p-1 px-2 text-base font-semibold
+                          hover:scale-105 duration-300'
+                >
+                  Login
+                </button>
+              </form>
             </div>
-        )
-   
-    
-       
+          </div>
+        </div>
+      </>
+    )
+  }
+
+  return (
+    <div className='bg-orange-400 min-h-screen flex'>
+
+      <Navbar showNav={showNav} setShowNav={setShowNav} />
+      <div className='bg-white flex-grow p-4'>
+        {children}
+      </div>
+    </div>
+  )
 }
 
 export default Layout;
